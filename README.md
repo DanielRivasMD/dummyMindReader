bdd4450162c90e830b7e04fe6bbd49a95e2ad4ca
DanielRivasMD <danielrivasmd@gmail.com>
2021-02-17 15:52:06 +0100

    dummy mind reader function

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://DanielRivasMD.github.io/dummyMindReader.jl/dev)
 [![Build Status](https://travis-ci.com/DanielRivasMD/dummyMindReader.jl.svg?branch=master)](https://travis-ci.com/DanielRivasMD/dummyMindReader.jl)
 [![Coverage](https://codecov.io/gh/DanielRivasMD/dummyMindReader.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/DanielRivasMD/dummyMindReader.jl)
+
+dummy CLI for Docker image construction
--- src/dummyMindReader.jl
+++ src/dummyMindReader.jl
@@ -1,5 +1,57 @@
 module dummyMindReader
 
-# Write your package code here.
+#  Write your package code here.
+
+################################################################################
+
+#  imports
+begin
+  using CairoMakie, AbstractPlotting # plotting packages
+  using CSV, DataFrames  # file manipulation
+  using ArgParse         # arg parser
+end
+
+################################################################################
+
+#  no documentation
+function shArgParser(args)
+  #  minimal argument parsing
+  s = ArgParseSettings(description = "dummy mind reader")
+  @add_arg_table! s begin
+      "--file", "-f"
+        arg_type = String
+        required = true
+        help = "csv file to read"
+      "--output", "-o"
+        arg_type = String
+        required = false
+        help = "output svg file"
+  end
+  parsed_args = parse_args(s)
+  return parsed_args
+end
+
+################################################################################
+
+#  parse shell arguments
+shArgs = shArgParser(ARGS)
+
+begin
+  file = shArgs["file"]
+  output = shArgs["output"]
+end
+
+################################################################################
+
+#  read file
+df = CSV.File(file, header = false) |> DataFrame
+
+#  plot
+scene = barplot(df[:, 2], color = :red, strokecolor = :black, strokewidth = 1)
+
+#  save plot
+save(output, scene, pt_per_unit = 0.5)
+
+################################################################################
 
 end
